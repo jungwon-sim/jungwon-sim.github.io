4f623b4948b90c5df575e2a782c33878dd42448f
catherine <wjddnjs960765@gamail.com>
2022-02-16 23:18:37 +0900

    대충

--- js/invitation.js
+++ js/invitation.js
@@ -8,7 +8,8 @@ $(document).ready(function(){
         var windowHT = $(window).height()
         $('#inv').width(windowWD);
         $('#inv').height(windowHT * 2);  
-        $('.b').css('margin-top',windowHT)   
+        $('.b').height(windowHT);  
+        $('.a').height(windowHT);  
     };
 
     console.log,WindowSize();
@@ -18,14 +19,27 @@ $(document).ready(function(){
     });
 
     var BackText = 'INVITAION';
-    var BackTextRe = BackText.repeat(800);
+    var BackTextRe = BackText.repeat(500);
     $('#inv').text(BackTextRe); 
 
+
+
+
+
     $('.next').click(function(){
+        var anText_sub1 = document.getElementById('who').value;
+        if(anText_sub1 == '배서진' ){
             $('.one').fadeOut();
             $('.two').fadeIn();     
-        
+        }else{
+            if(anText_sub1 == '서보람' ){
+            $('.one').fadeOut();
+            $('.two').fadeIn();
+            }else{
+                alert('초대받지 못한 휴먼입니다.</br>다음 기회에 도전하세요')}
+        }
     })
 
+    $('.place').html('장소</br>독산동 953 삼부르네상스 플러스 1020호</br>시간</br>언제오실텨?');
 
 })
